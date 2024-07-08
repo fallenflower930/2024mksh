27d5655dfc4ff8a229ab07c3821f411600c45b55
fallenflower930 <aspplan06@gmail.com>
2024-07-08 12:04:01 +0800

    day06

---
+++ opencv01_import_cv2_imread.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import cv2
+
+img = cv2.imread('onepiece.png')
+cv2.imshow('one', img)
+cv2.imshow('opencv01',img)
+cv2.imshow('mksh',img)
+
+cv2.waitKey(0 )
+#File-New Ctrl-N 開新檔案
+#File-save as 另存新檔
+
